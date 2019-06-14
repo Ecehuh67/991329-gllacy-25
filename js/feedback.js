52813dee823283c9dbfc00844bfbcdd28e11b80e
Kirill T <ecehuh67@gmail.com>
2019-06-14 16:06:35 +0300

    active form ready

---
+++ js/feedback.js
@@ -0,0 +1,36 @@
+var link = document.querySelector(".feedback-link");
+var popup = document.querySelector(".comments");
+var overlay = document.querySelector(".overlay");
+var close = popup.querySelector(".modal-close-main");
+var contactName = popup.querySelector("[name=first-name]");
+var form = popup.querySelector("form");
+var email = popup.querySelector("[name=email-for-call]");
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("comments-show");
+  overlay.classList.add("overlay-show");
+  contactName.focus();
+});
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("comments-show");
+  overlay.classList.remove("overlay-show");
+});
+
+form.addEventListener("submit", function(evt) {
+    if (!contactName.value || !email.value)
+    evt.preventDefault();
+    console.log("Заполните поля формы");
+});
+
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("comments-show") || overlay.classList.contains("overlay-show")) {
+            popup.classList.remove("comments-show");
+            overlay.classList.remove("overlay-show");
+        }
+    }
+});
